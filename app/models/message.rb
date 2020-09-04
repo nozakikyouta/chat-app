07b90f8561b84c98f3bf9936af1554e6ad0e4b29
nozakikyouta <tuebkbxhmb@icloud.com>
2020-09-04 12:10:34 +0900

    結合テストの実装

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,10 @@
 class Message < ApplicationRecord
   belongs_to:room
-  belongs_tp:user
+  belongs_to:user
   has_one_attacked:image
-  validates:content,presence:true
+  validates:content, presence: true, unless: :was_attached?
+
+  def was_attached?
+    self.image.attached?
+  end
 end
